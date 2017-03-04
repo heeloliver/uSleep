85885898a56f5fbcb54ff3da6b81ef3a65e309bf
Oliver Mulcahy <om@Olivers-MacBook-Pro.local>
2017-03-03 21:52:11 -0800

    first commit

--- uSleep/ViewController.swift
+++ uSleep/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uberTimeRemaining: UILabel!
+    @IBOutlet weak var sleepBenefits: UITextView!
+    @IBOutlet weak var sleepButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
