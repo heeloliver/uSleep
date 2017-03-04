9f193f53ea3ca867fa9491139a9c2be5e4360d4c
Oliver Mulcahy <om@Olivers-MacBook-Pro.local>
2017-03-03 20:46:13 -0800

    Initial Commit

---
+++ uSleep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uSleep
+//
+//  Created by Oliver Mulcahy on 3/3/17.
+//  Copyright © 2017 Oliver Mulcahy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
